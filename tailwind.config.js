757985730bf7e9070176af4bf43818ddd2cf0938
mungai michael <mungaimikey@gmail.com>
2024-02-19 13:57:49 +0300

    Deleted home file and fixed native wind support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*/.{ts,tsx}',
-    './app/**/*/.{ts,tsx}'
+    "./App.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
 
   ],
   theme: {
