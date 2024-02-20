cd8573bc4da87a10058120677ce1d70228eeaa5d
mungai michael <mungaimikey@gmail.com>
2024-02-20 11:04:03 +0300

    added favourite and library bottom tabs

---
+++ app/(tabs)/Favourites.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+export default function Favourites() {
+  return (
+    <View >
+      <Text>Tab Two</Text>
+      <View  lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </View>
+  );
+}
+
+
