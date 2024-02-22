13e6504fa7c747acf7fad05c40f0691016d35ca3
mungai michael <mungaimikey@gmail.com>
2024-02-22 10:15:56 +0300

    added the fetch hook fixing all the interface bugs

--- hooks/useDataFetch.ts
+++ hooks/useDataFetch.ts
@@ -1,38 +1,79 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-
-interface useDataFetchReturn {
-    loading: boolean, 
-    data:[]
+interface BookInfo {
+    key: string;
+    type: string;
+    seed: string[];
+    title: string;
+    title_suggest: string;
+    title_sort: string;
+    edition_count: number;
+    edition_key: string[];
+    publish_date: string[];
+    publish_year: number[];
+    first_publish_year: number;
+    isbn: string[];
+    last_modified_i: number;
+    ebook_count_i: number;
+    ebook_access: string;
+    has_fulltext: boolean;
+    public_scan_b: boolean;
+    cover_edition_key: string;
+    cover_i: number;
+    publisher: string[];
+    language: string[];
+    subject: string[];
+    publisher_facet: string[];
+    subject_facet: string[];
+    _version_: number;
+    subject_key: string[];
 }
 
-const useDataFetch = () => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false)
-    
-    
-    const fetchData = async ()  => {
-        try {
-            const response = await fetch('https://openlibrary.org/search.json?q=artificial + technology&limit=1&numFoundExact')
-            setLoading(true)
-            const data = await response.json()
-            
-            setData(data)
-            setLoading(false)
-        
-        }
-        catch (error) {
-            console.log(error)
-        
-      }
+interface UseDataFetchReturn {
+    numFound: number;
+    start: number;
+    numFoundExact: boolean; 
+    docs: BookInfo[];
+    num_found: number;
+    q: string;
+    offset: null;
+}
 
-    }
+interface UseDataFetchReturnInterface {
+    loading: boolean; 
+    data: UseDataFetchReturn;
+}
+
+const useDataFetch = (): UseDataFetchReturnInterface => {
+    const [data, setData] = useState<UseDataFetchReturn>({
+        numFound: 0,
+        start: 0,
+        numFoundExact: false,
+        docs: [],
+        num_found: 0,
+        q: "",
+        offset: null
+    });
+    const [loading, setLoading] = useState(false);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch('https://openlibrary.org/search.json?q=artificial+technology&limit=1&numFoundExact&limit=20');
+            const jsonData = await response.json();
+            setData(jsonData);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
         fetchData();
-    }, [])
+    }, []);
 
-    return {loading, data}
-}
+    return { loading, data };
+};
 
-export default useDataFetch; 
+export default useDataFetch;
