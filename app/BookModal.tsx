2b074ca2787a2ae2d422095497a07ae7129111a5
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-03-11 19:52:05 +0300

    fixed random ts errors

--- app/BookModal.tsx
+++ app/BookModal.tsx
@@ -1,7 +1,6 @@
 import { View, Text, Pressable, Image, ImageBackground } from 'react-native'
 import React, { useEffect } from 'react'
 import { useLocalSearchParams } from 'expo-router'
-import useImageFetch from '@/hooks/useImageFetch'
 // interface BookModalProps {
 //     // imageUrl: string, 
 //     title: string, 
@@ -15,29 +14,20 @@ import useImageFetch from '@/hooks/useImageFetch'
 const BookModal = () => {
   const { cover_i, title } = useLocalSearchParams()
 
-  const image = useImageFetch()
 
 
   return (
     <View
       className='flex-1 '
     >
-      {/* Image Section */}
-      <View
-        className="w-screen h-1/3 rounded-br-lg bg-slate-200"
-      >
-        <Image
-          source={{ uri: `https://covers.openlibrary.org/b/oclc/${image}-L.jpg` }} 
-          className='w-[100%] h-[100%] '
-        />
-      </View>
+
 
       {/* Title  Section */}
       <View
         className='h-20 bg-black flex-row justify-between items-center'
       >
         <Text>{title}</Text>
-        
+
       </View>
 
     </View>
