bc0b46a059009e8e8008ba73cf592310fd4e80e6
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-03-20 22:29:38 +0300

    Update index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ import Loader from '@/components/Home/Loader';
 
 export default function HomeScreen() {
 
-  const {data,loading} = useDataFetch()
+  const {data,loading} = useDataFetch("aliens")
 
   const colorScheme = useColorScheme()
 
@@ -24,47 +24,7 @@ export default function HomeScreen() {
   let genericContainer = 'flex-row justify-between w-[90%] mx-auto items-center mt-8'
 
 
-  // show spinner when loading 
-   const spinValue = useRef(new Animated.Value(0)).current;
 
-  useEffect(() => {
-    if (loading) {
-      Animated.loop(
-        Animated.timing(
-          spinValue,
-          {
-            toValue: 1,
-            duration: 2000,
-            easing: Easing.linear,
-            useNativeDriver: true
-          }
-        )
-      ).start();
-    } else {
-      spinValue.setValue(0);
-    }
-  }, [loading, spinValue]);
-
-    const spin = spinValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg']
-    });
-  
-  const SvgLoader = () => (
-    <Svg
-      width={50}
-      height={50}
-viewBox="0 0 24 24"
-    >
-  
-      <Path
-        strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-        stroke="#000000"
-        d="M12 3V6M12 18V21M6 12H3M21 12H18M5.63672 5.63672L7.75977 7.75977M16.2422 16.2422L18.3633 18.3633M18.3652 5.63477L16.2441 7.75586M7.75781 16.2422L5.63477 18.3652"
-      />
-    </Svg>
-  ); 
-  
   return (
     <View className="flex-1 relative ">
       <ScrollView>
