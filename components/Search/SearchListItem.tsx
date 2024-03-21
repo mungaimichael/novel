fc069f4f3ad10a937f0bfacd4faa459f858f7328
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-03-21 14:02:23 +0300

    added fallback when a component does not have an image

--- components/Search/SearchListItem.tsx
+++ components/Search/SearchListItem.tsx
@@ -1,31 +1,49 @@
 import { View, Text, Image } from 'react-native'
 import React, { FC } from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 interface SearchListProps {
-    cover_i?: number, 
-    author: string, 
-    title: string
+  cover_i?: number,
+  author?: string | string[],
+  title: string
 }
 
-const SearchListItem:FC<SearchListProps> = ({cover_i, author ,title}) => {
+const SearchListItem: FC<SearchListProps> = ({ cover_i, author, title }) => {
   return (
-      <View
-        className=" flex-row justify-start space-x-2"
-      >
-          <Image
-        source={{ uri: `https://covers.openlibrary.org/b/id/${cover_i}-L.jpg`}}
-              style={{width:60, height:70, objectFit:'contain'}}
-          />
+    <View
+      className=" flex-row justify-start items-end space-x-2 my-3 ml-4  w-full"
+    >
+      {
+        cover_i === undefined ? (
           <View
-            className="flex justify-start"
+            className=" bg-[#808080]/40 w-[50px] h-[80px] justify-center items-center"
           >
-              <Text>
-                  { title }
-              </Text>
-              <Text>
-                  {author}
-              </Text>
+            <MaterialCommunityIcons name="image-remove" size={20} color="#808080" />
           </View>
+        )
+          : (
+            <Image
+              source={{ uri: `https://covers.openlibrary.org/b/id/${cover_i}-L.jpg` }}
+              className=' w-[50px] h-full rounded-t-lg  '
+              style={{ resizeMode: 'contain' }}
+
+            />
+          )
+      }
+      <View
+        className="flex justify-start space-y-2"
+      >
+        <Text
+          className="font-regular w-2/3 "
+        >
+          {title}
+        </Text>
+        <Text
+          className="font-regular text-sm "
+        >
+          {author}
+        </Text>
+      </View>
     </View>
   )
 }
