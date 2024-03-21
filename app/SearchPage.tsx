23f87de1fa0d338bf82300de580e9e7ecfa744a4
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-03-21 14:02:03 +0300

    Fixed virtualized list error on the Flatlist

--- app/SearchPage.tsx
+++ app/SearchPage.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Pressable } from 'react-native'
+import { View, Text, ScrollView, Pressable, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Input from '@/components/Search/Input'
 import { Ionicons } from '@expo/vector-icons'
@@ -7,31 +7,21 @@ import useDataFetch from '@/hooks/useDataFetch'
 import SearchListItem from '@/components/Search/SearchListItem'
 
 const SearchPage: React.FC = () => {
-
-
     const [searchValue, setSearchValue] = useState<string>('')
-
+    const { data, loading } = useDataFetch('java')
     const [recentSearches, setRecentSearches] = useState<{ text: string, color: string }[]>([])
-    
-    const addSearches = (value: string) => {
-        const newSearch = { text: value, color: rand() };
-        setRecentSearches(prevSearches => [newSearch, ...prevSearches]);
-    }
 
+    const addSearches = (value: string) => {
+        const newSearch = { text: value, color: rand() }
+        setRecentSearches(prevSearches => [newSearch, ...prevSearches])
+    }
 
     const rand = (): string => {
-        const randomHue = Math.floor(Math.random() * 360); // Random hue value
-        const randomSaturation = Math.floor(Math.random() * 50) + 10; // Random saturation between 50 and 100
-        const randomLightness = Math.floor(Math.random() * 20) + 30; // Random lightness between 60 and 80 for readability
-
-        return `hsl(${randomHue}, ${randomSaturation}%, ${randomLightness}%)`;
+        const randomHue = Math.floor(Math.random() * 360) // Random hue value
+        const randomSaturation = Math.floor(Math.random() * 50) + 10 // Random saturation between 50 and 100
+        const randomLightness = Math.floor(Math.random() * 20) + 30 // Random lightness between 60 and 80 for readability
+        return `hsl(${randomHue}, ${randomSaturation}%, ${randomLightness}%)`
     }
-
-
-    const {data, loading} = useDataFetch('java')
-
-
-
 
     useEffect(() => {
         // Generate initial colors for existing recent searches
@@ -39,49 +29,42 @@ const SearchPage: React.FC = () => {
             return prevSearches.map(search => ({
                 ...search,
                 color: rand()
-            }));
-        });
-    }, []);
+            }))
+        })
+    }, [])
 
     return (
-        <
+        <FlatList
 
-            >
-            <ScrollView
-                className=""
-
-            >
-
-                {/* Input Section */}
-                <View
-                    className=" bg-slate-400/5 border-0 justify-center items-center h-[100px] relative"
-                >
-                    <Input
-                        type='text'
-                        onChangeText={(text: string) => setSearchValue(text)}
-                    />
-                    <Pressable
-                        onPress={() => { addSearches(searchValue); console.log(loading ? loading :  searchValue,data) }}
-                        className="absolute right-5 bg-slate-800/70 w-14 h-[48] top-[36] flex justify-center items-center rounded-lg"
+            ListHeaderComponent={() => (
+                <ScrollView>
+                    {/* Input Section */}
+                    <View
+                        className=" bg-slate-400/5 border-0 justify-center items-center h-[100px] relative"
                     >
-                        <Ionicons
-                            name='search'
-                            color="#fff"
-                            size={20}
+                        <Input
+                            type='text'
+                            onChangeText={(text: string) => setSearchValue(text)}
                         />
-                    </Pressable>
+                        <Pressable
+                            onPress={() => { addSearches(searchValue); console.log(loading ? loading : searchValue, data) }}
+                            className="absolute right-5 bg-slate-800/70 w-14 h-[48] top-[36] flex justify-center items-center rounded-lg"
+                        >
+                            <Ionicons
+                                name='search'
+                                color="#fff"
+                                size={20}
+                            />
+                        </Pressable>
+                    </View>
 
-                </View>
-
-                {/* Recent Searches  */}
-                <View
-                    className="w-[90%] mx-auto  flex-row justify-start space-x-4 space-y-1 items-center flex-wrap flex-grow "
-                >
-                    {
-                        recentSearches.map(({color,text}, index) => (
+                    {/* Recent Searches  */}
+                    <View
+                        className="w-[90%] mx-auto mb-3  flex-row justify-start space-x-4 space-y-1 items-center flex-wrap flex-grow "
+                    >
+                        {recentSearches.map(({ color, text }, index) => (
                             <Pressable
                                 key={index}
-
                             >
                                 <Text
                                     className="font-regular text-[16px] px-2 py-1 text-white bg-opacity-5 rounded-lg"
@@ -90,33 +73,21 @@ const SearchPage: React.FC = () => {
                                     {text}
                                 </Text>
                             </Pressable>
-                        ))
+                        ))}
+                    </View>
+                </ScrollView>
+            )}
 
-                        }
-                </View>
-
-                {/* Searched Books section  */}
-
-                <View
-                    className="w-[90%] flex-row flex-wrap justify-start space-x-8"
-                >
-                    {
-                        data && data.docs.map(({title, author_name, cover_i}, index) => (
-                            <SearchListItem
-                                key={index}
-                                title={title}
-                                author={author_name}
-                                coverUrl={cover_i}
-                                
-                            />
-                        ))
-                        }
-                </View>
-
-            </ScrollView>
-
-
-        </>
+            data={data.docs}
+            renderItem={({ item, index }) => (
+                <SearchListItem
+                    key={index}
+                    title={item.title}
+                    author={item.author_name}
+                    cover_i={item.cover_i}
+                />
+            )}
+        />
     )
 }
 
