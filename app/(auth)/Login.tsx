123cc761457b820f3e7a70b754eb669b88001967
mungai michael <83639621+mungaimichael@users.noreply.github.com>
2024-03-30 15:15:50 +0300

    Created the Login screen

---
+++ app/(auth)/Login.tsx
@@ -0,0 +1,63 @@
+import { useRouter } from 'expo-router';
+import React, { FC } from 'react'
+import { Image, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+const Login: FC = () => {
+    const router = useRouter();
+    const images = [require('@/assets/images/6.jpg'), require('@/assets/images/1.jpg'), require('@/assets/images/2.webp'), require('@/assets/images/1.jpg'), require('@/assets/images/3.jpeg'), require('@/assets/images/6.jpg'), require('@/assets/images/1.jpg'), require('@/assets/images/3.jpeg'), require('@/assets/images/4.webp')];
+    return (
+        <SafeAreaView
+            className="flex-1 "
+        >
+            <View
+                className="flex-1  mt-[20%]"
+
+            >
+
+                <View className=" flex-row space-x-1 justify-between flex-wrap h-2/2">
+                    {images.map((image, index) => (
+                        <View
+                            className="h-300"
+                            key={index}>
+                            <Image
+                                source={image}
+                                className="h-[120] w-[80] object-cover m-3"
+                            />
+                        </View>
+                    ))}
+                </View>
+                <View
+                    className="w-[90%] mx-auto items-center mt-4  "
+                    >
+                    <View
+                        className='w-5/6 ml-2 flex space-y-3'
+                    >
+                        <Text
+                            className="text-3xl font-semiBold capitalize "
+                        >
+                            Unlock the power of digital literature
+                        </Text>
+                        <Text>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, molestiae. Voluptatem officiis ipsam, reiciendis magnam
+                        </Text>
+                    </View>
+            
+                    <Pressable
+                        onPress={()=>router.navigate('SignUp')}
+                            className="w-5/6 mt-3 bg-green-500/80 h-[40] flex justify-center items-center"
+                        ><Text
+                            className="font-regular text-md text-white"
+                        >Get Started</Text></Pressable>
+                    <Pressable
+                        className="w-5/6 mt-1 bg-slate-200/90 h-[40] flex justify-center items-center"
+                    ><Text
+                        className="font-semiBold text-md"
+                    >Login</Text></Pressable>
+                    </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+export default Login
